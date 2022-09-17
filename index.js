a51918ee339f947a59fc4b5318e237b9d8d7fa56
Louis B <barretolouis5@gmail.com>
2022-09-17 00:58:36 -0500

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your Code Here
+let userName = window.prompt('Please enter name:')
+// Add userScore
+let userScore = 0
+// Add playAgain
+let playAgain = true
+
+//Answer each question in the Array
+function playGame (){
+
+for (let i = 0; i < questions.length; i ++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if (userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
+}
+
+window.alert('Your score is ' +userScore)
+}
+//add option to play again 
+while (playAgain === true) {
+    playGame()
+    let userChoice = window.prompt ('Would you like to play again? Answer yes or no.')
+    if(userChoice === 'yes' ) {
+        playAgain = true
+    } else {
+        playAgain = false
+        window.alert('Thanks for playing my Game!') 
+           
+    }
+}
+
